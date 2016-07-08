c90fbe6353610184ebb53950e446b29e2091df6c
jun <1042439471@qq.com>
2016-07-08 17:22:50 +0800

    request with proxy

--- HttpUtils/src/main/java/com/jun/httpUtils/HttpConnectionManager.java
+++ HttpUtils/src/main/java/com/jun/httpUtils/HttpConnectionManager.java
@@ -1,16 +1,23 @@
 package com.jun.httpUtils;
 
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthSchemeProvider;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.AuthenticationStrategy;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.AuthSchemes;
 import org.apache.http.client.config.CookieSpecs;
 import org.apache.http.client.config.RequestConfig;
+import org.apache.http.config.Lookup;
 import org.apache.http.config.Registry;
 import org.apache.http.config.RegistryBuilder;
 import org.apache.http.conn.socket.ConnectionSocketFactory;
 import org.apache.http.conn.socket.PlainConnectionSocketFactory;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.auth.BasicSchemeFactory;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.client.LaxRedirectStrategy;
@@ -60,7 +67,25 @@ public class HttpConnectionManager {
         return httpClient;
     }
 
-    public  HttpClient getHtpClient(HttpHost proxy){
+    /**
+     * 默认是 Bsic认证机制
+     * @param ip
+     * @param username
+     * @param password
+     * @return
+     */
+    public HttpClient getHtpClient(String ip,int port,String username,String password) {
+        HttpHost proxy=new HttpHost(ip,port);
+        Lookup<AuthSchemeProvider> authProviders =
+                RegistryBuilder.<AuthSchemeProvider>create()
+                        .register(AuthSchemes.BASIC, new BasicSchemeFactory())
+                        .build();
+        BasicCredentialsProvider credsProvider = new BasicCredentialsProvider();
+        if(username!=null && password!=null){
+            credsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+        } else {
+            credsProvider.setCredentials(AuthScope.ANY,null);
+        }
 
         RequestConfig requestConfig = RequestConfig.custom().setCookieSpec(CookieSpecs.STANDARD_STRICT).build();
         CloseableHttpClient httpClient = HttpClients
@@ -69,6 +94,8 @@ public class HttpConnectionManager {
                 .setProxy(proxy)
                 .setRedirectStrategy(new LaxRedirectStrategy())
                 .setDefaultRequestConfig(requestConfig)
+                .setDefaultAuthSchemeRegistry(authProviders)
+                .setDefaultCredentialsProvider(credsProvider)
                 .build();
         return httpClient;
     }
