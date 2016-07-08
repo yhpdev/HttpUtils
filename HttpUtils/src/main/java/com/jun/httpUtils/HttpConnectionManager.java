a0957479ddb83b5e6f057d67ca9c0f169072cfe1
jun <1042439471@qq.com>
2016-07-08 10:39:58 +0800

    request with proxy

--- HttpUtils/src/main/java/com/jun/httpUtils/HttpConnectionManager.java
+++ HttpUtils/src/main/java/com/jun/httpUtils/HttpConnectionManager.java
@@ -1,32 +1,21 @@
 package com.jun.httpUtils;
 
 import org.apache.http.HttpHost;
-import org.apache.http.client.CookieStore;
+import org.apache.http.client.AuthenticationStrategy;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.CookieSpecs;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.config.Registry;
 import org.apache.http.config.RegistryBuilder;
-import org.apache.http.conn.routing.HttpRoute;
-import org.apache.http.conn.scheme.PlainSocketFactory;
-import org.apache.http.conn.scheme.Scheme;
-import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.conn.socket.ConnectionSocketFactory;
-import org.apache.http.conn.socket.LayeredConnectionSocketFactory;
 import org.apache.http.conn.socket.PlainConnectionSocketFactory;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
-import org.apache.http.cookie.Cookie;
-import org.apache.http.impl.client.*;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.LaxRedirectStrategy;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
-import org.apache.http.impl.cookie.BasicClientCookie;
 import org.apache.http.ssl.SSLContextBuilder;
-import org.apache.http.ssl.SSLContexts;
-
-import javax.net.ssl.SSLContext;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
 
 /**
  * Created by jun on 16/7/2.
@@ -70,4 +59,31 @@ public class HttpConnectionManager {
                 .build();
         return httpClient;
     }
+
+    public  HttpClient getHtpClient(HttpHost proxy){
+
+        RequestConfig requestConfig = RequestConfig.custom().setCookieSpec(CookieSpecs.STANDARD_STRICT).build();
+        CloseableHttpClient httpClient = HttpClients
+                .custom()
+                .setConnectionManager(cm)
+                .setProxy(proxy)
+                .setRedirectStrategy(new LaxRedirectStrategy())
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+        return httpClient;
+    }
+    public  HttpClient getHtpClient(AuthenticationStrategy proxy){
+
+        RequestConfig requestConfig = RequestConfig.custom().setCookieSpec(CookieSpecs.STANDARD_STRICT).build();
+        CloseableHttpClient httpClient = HttpClients
+                .custom()
+                .setConnectionManager(cm)
+                .setProxyAuthenticationStrategy(proxy)
+                .setRedirectStrategy(new LaxRedirectStrategy())
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+        return httpClient;
+    }
+
+
 }
